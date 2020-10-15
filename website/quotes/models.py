a0f0ca59ffd98fa8a5da85bb1e42691e0d42879e
cruizk <cruiz.kusman@gmail.com>
2020-10-15 15:32:36 -0500

    Added most of the tutorial

--- website/quotes/models.py
+++ website/quotes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+#class Quote(models.Model):
+#  quote_text = models.CharField(max_length=500)
+#  quote_author = models.CharField(max_length=200)
